509355d4de706e9b4f3eca3fa97cfb23468d7bee
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2025-07-31 15:10:23 +0700

    fix: adjust unit test

--- server.js
+++ server.js
@@ -10,16 +10,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-connectDB();
-
 app.get("/", (req, res) => {
   res.send("Hello World! Ini test Insignia!");
 });
 
 app.use("/users", userRoutes);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+if (require.main === module) {
+  const connectDB = require("./config/connection");
+
+  connectDB();
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
 
 module.exports = app;
